f0708a1e72d12d84be5fa656667266d66dfafc25
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-05-01 18:16:56 -0500

    Finished 2c

--- Q1.py
+++ Q1.py
@@ -1,42 +1,79 @@
-from sklearn.neural_network import MLPClassifier
+from typing import Tuple
+from math import log
 from csv import reader
 
-def getXy(csv: str):
-    # Use pythons built-in csv parser to load rraining data
+from sklearn.neural_network import MLPClassifier
+
+# Function for getting X and y from csv
+def getXy(csv: str) -> Tuple[list]:
+    # Use pythons built-in csv parser
     with open(csv, "r") as csv:
         read = reader(csv)
         # [1:] to chop off the title row
         training = list(read)[1:]
 
-    # Split training into X and y
+    # Split into X and y
     X = [i[:-1] for i in training]
-    y = [i[11] for i in training]
+    y = [int(i[11]) for i in training]
 
     return X, y
 
-# 2a)
-one_hidden_20_units = MLPClassifier(solver = 'lbfgs', alpha = 1e-5, hidden_layer_sizes = (20), random_state = 1)
-two_hidden_10_units = MLPClassifier(solver = 'lbfgs', alpha = 1e-5, hidden_layer_sizes = (10, 10), random_state = 1)
+# Fucntion for cross-entropy
+def crossEntropy(h: list, c: list) -> float:
+    ce = 0
 
-#   Load training data
+    for i in range(len(h)):
+        ce += c[i] * log(h[i][0]) + (1 - c[i]) * log(1 - h[i][0])
+
+    return ce / -len(h)
+
+"""
+ 2a)
+"""
+one_hidden_20_units = MLPClassifier(hidden_layer_sizes = (20), random_state = 1)
+two_hidden_10_units = MLPClassifier(hidden_layer_sizes = (10, 10), random_state = 1)
+
+# Load training data
 training = getXy("data/training.csv")
 X = training[0]
 y = training[1]
 
-#   Fit models
+# Fit models
 one_hidden_20_units.fit(X, y)
 two_hidden_10_units.fit(X, y)
 
-# 2b)
-#   Load validation data
+"""
+ 2b)
+"""
+# Load validation data
 validation = getXy("data/validation.csv")
 X_v = validation[0]
 y_v = validation[1]
 
-#   Make probabilistic predictions with predict_proba
+# Make probabilistic predictions with predict_proba
 first_preds = one_hidden_20_units.predict_proba(X_v)
 second_preds = two_hidden_10_units.predict_proba(X_v)
 
-print(first_preds)
-print()
-print(second_preds)
+print(f"First model cross entropy: {crossEntropy(first_preds, y_v)}")
+print(f"Second model cross entropy: {crossEntropy(second_preds, y_v)}")
+
+"""
+ 2c)
+"""
+# Second network appears to preform better
+# Combine training and validation
+X_b = X
+y_b = y
+X_b.extend(X_v)
+y_b.extend(y_v)
+
+new_network = MLPClassifier(hidden_layer_sizes = (10, 10), random_state = 1)
+new_network.fit(X_b, y_b)
+
+testing = getXy("data/testing.csv")
+X_t = testing[0]
+y_t = testing[1]
+
+# Make new predictions
+new_preds = new_network.predict_proba(X_t)
+print(f"New model cross entropy: {crossEntropy(new_preds, y_t)}")
