829d78108692f7eeaee901e42e9ca78a25ffa610
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-05-01 20:07:47 -0500

    Finished 3e

--- Q2.py
+++ Q2.py
@@ -3,6 +3,8 @@ from math import log
 from csv import reader
 
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import log_loss
+
 
 # Function for getting X and y from csv
 def getXy(csv: str) -> Tuple[list]:
@@ -28,35 +30,58 @@ def crossEntropy(h: list, c: list) -> float:
     return ce / -len(h)
 
 """
-3a)
+Load data
 """
 # Load training data
 training = getXy("data/training.csv")
 X = training[0]
 y = training[1]
 
-gini = DecisionTreeClassifier(criterion = "gini", max_depth = 5)
-gini.fit(X, y)
-
-"""
-3b)
-"""
 # Load validation data
 validation = getXy("data/validation.csv")
 X_v = validation[0]
 y_v = validation[1]
 
+# Combine training and validation
+X_b = X
+y_b = y
+X_b.extend(X_v)
+y_b.extend(y_v)
+
+# Load testing data
+testing = getXy("data/testing.csv")
+X_t = testing[0]
+y_t = testing[1]
+"""
+3a)
+"""
+gini = DecisionTreeClassifier(criterion = "gini", max_depth = 5, random_state = 1)
+gini.fit(X, y)
+
+"""
+3b)
+"""
 val_preds = gini.predict_proba(X_v)
-print(f"Cross entropy: {crossEntropy(val_preds, y_v)}") # log(0) error what the frick bro 81 instances like that
+print(f"Cross entropy for gini: {log_loss(y_v, val_preds)}") # log(0) error what the frick bro 81 instances like that
 
 """
 3c) 
 """
-info = DecisionTreeClassifier(criterion = "entropy", max_depth = 5)
+info = DecisionTreeClassifier(criterion = "entropy", max_depth = 5, random_state = 1)
 info.fit(X, y)
 
 """
 3d)
 """
 info_val_preds = info.predict_proba(X_v)
-print(f"Cross entropy: {crossEntropy(info_val_preds, y_v)}")
+print(f"Cross entropy for info: {log_loss(y_v, info_val_preds)}")
+
+"""
+3e)
+"""
+# Info works better
+comb = DecisionTreeClassifier(criterion = "entropy", max_depth = 5, random_state = 1)
+comb.fit(X_b, y_b)
+
+comb_val_preds = comb.predict_proba(X_t)
+print(f"Cross entropy for comb: {log_loss(y_t, comb_val_preds)}")
